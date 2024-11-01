ddab20b524d2516e22d3b1393956ec3886f1738d
Carlos Zambrano <ceze21@gmail.com>
2024-11-01 14:48:40 +0100

    agregando login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SuccessComponent } from './success/success.component';
+import { AuthGuard } from './auth.guard';
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'success', component: SuccessComponent, canActivate: [AuthGuard] } 
+  ];
+  
